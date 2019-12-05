4cc4b8ca4180cc3b8c75e41ecc4f5cde5c5ae57b
Matt Segvich <msegvich@pivotal.io>
2019-12-05 11:08:35 -0600

    Fixed upgrade script

--- my_fly.sh
+++ my_fly.sh
@@ -2,6 +2,6 @@
 
 #GCP
 ./1-fly-install-opsman.sh -t s1p -p dev -n install-opsman -o ops-files/resource-gcs.yml
-./2-fly-upgrade-opsman.sh -t s1p -p dev -n upgrade-opsman -o ops-files/upgrade-resource-gcs.yml ops-files/task-apply-changes.yml
+./2-fly-upgrade-opsman.sh -t s1p -p dev -n upgrade-opsman -o ops-files/upgrade-resource-gcs.yml
 ./3-fly-install-upgrade-products.sh -t s1p -p dev -n install-upgrade-products -o ops-files/resource-gcs.yml
 ./3-fly-install-upgrade-products.sh -t s1p -p dev -n install-upgrade-products -o ops-files/resource-gcs.yml ops-files/task-apply-changes.yml
