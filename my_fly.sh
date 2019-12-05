8becb366558304c2a137eaf8cb6a3a2cb63ed998
Matt Segvich <msegvich@pivotal.io>
2019-12-05 10:33:02 -0600

    Updated scripts and products

--- my_fly.sh
+++ my_fly.sh
@@ -2,4 +2,6 @@
 
 #GCP
 ./1-fly-install-opsman.sh -t s1p -p dev -n install-opsman -o ops-files/resource-gcs.yml
-./3-fly-install-upgrade-products.sh -t dev -p dev -n install-upgrade-products
+./2-fly-upgrade-opsman.sh -t s1p -p dev -n upgrade-opsman -o ops-files/resource-gcs.yml ops-files/task-apply-changes.yml
+./3-fly-install-upgrade-products.sh -t s1p -p dev -n install-upgrade-products -o ops-files/resource-gcs.yml
+./3-fly-install-upgrade-products.sh -t s1p -p dev -n install-upgrade-products -o ops-files/resource-gcs.yml ops-files/task-apply-changes.yml
