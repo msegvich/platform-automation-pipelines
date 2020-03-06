8ec50519c4624cc0f2522717962111b62934d0dd
Matt Segvich <msegvich@pivotal.io>
2020-03-06 10:01:03 -0600

    Changes for new domain

--- my_fly.sh
+++ my_fly.sh
@@ -5,3 +5,4 @@
 ./2-fly-upgrade-opsman.sh -t s1p -p dev -n upgrade-opsman -o ops-files/upgrade-resource-gcs.yml
 ./3-fly-install-upgrade-products.sh -t s1p -p dev -n install-upgrade-products -o ops-files/resource-gcs.yml
 ./3-fly-install-upgrade-products.sh -t s1p -p dev -n install-upgrade-products -o ops-files/resource-gcs.yml ops-files/task-apply-changes.yml
+./4-fly-patch-products.sh -t s1p -p dev -n patch-products -o ops-files/resource-gcs.yml
